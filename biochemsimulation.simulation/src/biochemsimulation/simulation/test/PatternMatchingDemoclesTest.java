0b3c8dd276a6f8a8d344d206465c0e9d384bcf28
arg0n1s <basti_seppl@yahoo.com>
2018-07-25 18:13:33 +0200

    Merge branch 'devel-democles' into origin
    -> removed unused imports

--- biochemsimulation.simulation/src/biochemsimulation/simulation/test/PatternMatchingDemoclesTest.java
+++ biochemsimulation.simulation/src/biochemsimulation/simulation/test/PatternMatchingDemoclesTest.java
@@ -1,9 +1,6 @@
 package biochemsimulation.simulation.test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import biochemsimulation.simulation.matching.PatternMatchingEngineEnum;
-import biochemsimulation.simulation.matching.PatternMatchingEngineFactory;
 
 class PatternMatchingDemoclesTest extends PatternMatchingTest {
 	
--- biochemsimulation.simulation/src/biochemsimulation/simulation/test/PatternMatchingViatraTest.java
+++ biochemsimulation.simulation/src/biochemsimulation/simulation/test/PatternMatchingViatraTest.java
@@ -1,12 +1,6 @@
 package biochemsimulation.simulation.test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
 import biochemsimulation.simulation.matching.PatternMatchingEngineEnum;
-import biochemsimulation.simulation.matching.PatternMatchingEngineFactory;
 
 class PatternMatchingViatraTest extends PatternMatchingTest {
 	
