1c6d52fb1e001e7f9bb7db8db6ace902a43053e9
Sebastian E <sebastian.ehmes@gmail.com>
2019-04-04 16:58:40 +0200

    # integrating eMoflon::IBeX #2

--- org.simsg.examples/src/org/simsg/examples/devel/Sandbox.java
+++ org.simsg.examples/src/org/simsg/examples/devel/Sandbox.java
@@ -1,6 +1,8 @@
 package org.simsg.examples.devel;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
@@ -11,7 +13,16 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.emoflon.ibex.gt.api.GraphTransformationAPI;
+import org.emoflon.ibex.gt.api.GraphTransformationApp;
+import org.emoflon.ibex.gt.api.GraphTransformationMatch;
+import org.emoflon.ibex.gt.api.GraphTransformationPattern;
 import org.simsg.core.utils.PersistenceUtils;
+import org.simsg.examples.devel.api.DevelAPI;
+import org.simsg.examples.devel.api.DevelApp;
+import org.simsg.examples.devel.api.DevelDemoclesApp;
+import org.simsg.examples.devel.api.rules.AgentPattern;
+import org.simsg.examples.devel.api.rules.KTxPattern;
 
 import GTLanguage.GTLanguagePackage;
 import GTLanguage.GTRuleSet;
@@ -19,20 +30,96 @@ import IBeXLanguage.IBeXLanguagePackage;
 import IBeXLanguage.IBeXPatternSet;
 import SimulationDefinition.SimDefinition;
 import SimulationDefinition.SimulationDefinitionFactory;
+import org.simsg.core.persistence.*;
 
 public class Sandbox {
 
 	public static void main(String[] args) {
+		//loadAModel("Goldbeter_Koshland");
+		//generateSimDef();
+		try {
+			// Load generated DemoclesApp-class by class name
+			Class<GraphTransformationApp<?>> democlesAppClass = (Class<GraphTransformationApp<?>>)java.lang.Class.forName("org.simsg.examples.devel.api.DevelDemoclesApp");
+			// Create dynamic instance of Democles-App class
+			GraphTransformationApp<?> democlesApp = democlesAppClass.newInstance();
+			// Load generated App-class by class name
+			Class<GraphTransformationApp<?>> appClass = (Class<GraphTransformationApp<?>>)java.lang.Class.forName("org.simsg.examples.devel.api.DevelApp");
+			// Create dynamic registerMetaModels()-Method of the App-class which is the DemoclesApp super class
+			Method method1 = appClass.getDeclaredMethod("registerMetaModels");
+			// Invoke dynamically create method
+			method1.invoke(democlesApp);
+			democlesApp.getModel().getResources().add(loadAModel("Goldbeter_Koshland"));
+			GraphTransformationAPI api = democlesApp.initAPI();
+			api.updateMatches();
+			
+			// Load generated API-class by class name
+			Class<GraphTransformationAPI> apiClass = (Class<GraphTransformationAPI>)java.lang.Class.forName("org.simsg.examples.devel.api.DevelAPI");
+			// Create dynamic agent()-Method of the API-class
+			Method method2 = apiClass.getDeclaredMethod("agent");
+			// Invoke dynamically create method
+			GraphTransformationPattern<?,?> p = (GraphTransformationPattern<?,?>)method2.invoke(api);
+			// Get matches -> ..  profit!
+			int count = p.findMatches().size();
+			System.out.println("Count: "+count);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		URI gtRulesURI = URI.createFileURI("F:\\Eclipse Workspaces\\SimSG\\org.simsg.examples\\src-gen\\org\\simsg\\examples\\devel\\api\\gt-rules.xmi");
-		URI ibexPatternsURI = URI.createFileURI("F:\\Eclipse Workspaces\\SimSG\\org.simsg.examples\\src-gen\\org\\simsg\\examples\\devel\\api\\ibex-patterns.xmi");
-		URI simModelURI = URI.createFileURI("F:\\Eclipse Workspaces\\SimSG\\org.simsg.examples\\models\\ContainerModels\\Goldbeter_Koshland.xmi");
-		URI simMetamodelURI = URI.createFileURI("F:\\Eclipse Workspaces\\SimSG\\org.simsg.examples\\models\\ContainerMetaModels\\Goldbeter_Koshland.ecore");
+		/*
+		DevelDemoclesApp dda = new DevelDemoclesApp();
+		dda.registerMetaModels();
+		dda.getModel().getResources().add(loadAModel("Goldbeter_Koshland"));
+		DevelAPI api2 = dda.initAPI();
+		api2.updateMatches();
+		System.out.println("Count: "+api2.KTx().findMatches().size());
+		System.out.println("Count: "+api2.agent().findMatches().size());
+		*/
+	}
+	
+	public static void generateSimDef() {
+		URI gtRulesURI = URI.createFileURI("src-gen\\org\\simsg\\examples\\devel\\api\\gt-rules.xmi");
+		URI ibexPatternsURI = URI.createFileURI("src-gen\\org\\simsg\\examples\\devel\\api\\ibex-patterns.xmi");
+		URI simModelURI = URI.createFileURI("models\\SimulationModels\\Goldbeter_Koshland.xmi");
+		URI simMetamodelURI = URI.createFileURI("models\\SimulationMetaModels\\Goldbeter_Koshland.ecore");
 		
 		SimDefinition def = createSimulationDefinition("Goldbeter_Koshland", gtRulesURI, ibexPatternsURI, simModelURI, simMetamodelURI);
 		
-		URI simDefinitionURI = URI.createFileURI("F:\\Eclipse Workspaces\\SimSG\\org.simsg.examples\\models\\SimDefTest.xmi");
+		URI simDefinitionURI = URI.createFileURI("models\\SimDefTest.xmi");
 		saveDefinition(def, simDefinitionURI);
+	}
+	
+	public static Resource loadAModel(String model) {
+		PersistenceManager pm = new SimplePersistenceManager();
+		pm.setModelFolderPath(System.getProperty("user.dir")+"//models");
+		pm.init();
+		try {
+			pm.loadSimulationDefinition("Goldbeter_Koshland");
+			return pm.loadSimulationModel("Goldbeter_Koshland");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	public static SimDefinition createSimulationDefinition(String modelName, URI gtRulesURI, URI ibexPatternsURI, URI simModelURI, URI simMetamodelURI) {
