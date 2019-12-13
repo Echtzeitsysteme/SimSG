ca7dfde008299f5aeb1fb422d1bd662f2efd8bf6
SebastianE <sebastian.ehmes@gmail.com>
2019-12-13 17:19:50 +0100

    more util files

---
+++ org.simsg.ui/src/org/simsg/ui/handler/NoActionCommandHandler.java
@@ -0,0 +1,22 @@
+package org.simsg.ui.handler;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.moflon.core.ui.AbstractCommandHandler;
+
+/**
+ * Null implementation of {@link AbstractCommandHandler}
+ * 
+ * @author Roland Kluge - Initial implementation
+ */
+public class NoActionCommandHandler extends AbstractCommandHandler {
+
+	/**
+	 * Does nothing
+	 */
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		return AbstractCommandHandler.DEFAULT_HANDLER_RESULT;
+	}
+
+}
---
+++ org.simsg.ui/src/org/simsg/ui/handler/ReconfigureLoggingHandler.java
@@ -0,0 +1,19 @@
+package org.simsg.ui.handler;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.moflon.core.ui.AbstractCommandHandler;
+import org.moflon.core.ui.MoflonCoreUiActivator;
+
+/**
+ * Triggers a reload of the logging configuration.
+ */
+public class ReconfigureLoggingHandler extends AbstractCommandHandler {
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		MoflonCoreUiActivator.getDefault().reconfigureLogging();
+		return null;
+	}
+
+}
