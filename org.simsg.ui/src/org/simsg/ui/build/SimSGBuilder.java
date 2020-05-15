1de925f39be2ee4a88beeca0ea33df4a48d3157a
tobnie <niehues.tobias@gmail.com>
2020-05-15 13:03:34 +0200

    refactoring

--- org.simsg.ui/src/org/simsg/ui/build/SimSGBuilder.java
+++ org.simsg.ui/src/org/simsg/ui/build/SimSGBuilder.java
@@ -60,17 +60,32 @@ import org.moflon.emf.codegen.resource.GenModelResource;
  */
 public class SimSGBuilder extends IncrementalProjectBuilder {
 	public static final Logger logger = Logger.getLogger(SimSGBuilder.class);
-	
+
 	public static final String DEFAULT_SRC_LOCATION = "src";
 	public static final String DEFAULT_METAMODEL_LOCATION = "model";
-	public static final String DEFAULT_MODEL_LOCATION = "instances/simulation_instances";
+
+	public static final String DEFAULT_INSTANCES_LOCATION = "instances";
 	public static final String DEFAULT_DEFINITION_LOCATION = "instances/simulation_definitions";
+	public static final String DEFAULT_MODEL_LOCATION = "instances/simulation_instances";
+	public static final String DEFAULT_RESULTS_LOCATION = "instances/simulation_results";
+
+	public final static String PACKAGE_IBEX_COMMON = "org.emoflon.ibex.common";
+	public final static String PACKAGE_IBEX_GT = "org.emoflon.ibex.gt";
+	public final static String PACKAGE_SIMSG_CORE = "org.simsg.core";
+	public final static String PACKAGE_SIMULATION_DEFINITION = "SimulationDefinition";
 
 	private static final String SIMSG_BUILDER_ID = "org.simsg.build.SimSGBuilder";
-	
+
 	private IFolder srcFolder;
 	private IFolder metaModelFolder;
 //	private IFolder modelFolder;
+
+	private IProject project;
+	private String projectName;
+	private String projectGenFolder;
+	private String apiResourcesLocation;
+	private String gtRulesLocation;
+	private String ibexPatternsLocation;
 
 	/**
 	 * Initializes the visitor condition
@@ -80,33 +95,40 @@ public class SimSGBuilder extends IncrementalProjectBuilder {
 	public SimSGBuilder() {
 	}
 	
+	private void setConstants(IProject project) {
+		projectName = project.getName();
+		projectGenFolder = projectName + "/gen/";
+		apiResourcesLocation = "src-gen/" + projectName.replace(".", "/") + "/api";
+		gtRulesLocation = apiResourcesLocation + "/gt-rules.xmi";
+		ibexPatternsLocation = apiResourcesLocation + "/ibex-patterns.xmi";
+	}
+
 	@Override
 	protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
 		IProject project = getProject();
+		setConstants(project);
 		final SubMonitor subMon = SubMonitor.convert(monitor, "Building SimSGProject: " + project, 1);
-		
-		if(kind == FULL_BUILD || kind == CLEAN_BUILD ) {
+
+		if (kind == FULL_BUILD || kind == CLEAN_BUILD) {
 			fullBuild(project, monitor);
 			project.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor());
 		}
 		subMon.worked(1);
-		
+
 		return null;
 	}
 
 	public static String getId() {
 		return SIMSG_BUILDER_ID;
 	}
-	
+
 	protected void fullBuild(final IProject project, final IProgressMonitor monitor) throws CoreException {
 		final SubMonitor subMon = SubMonitor.convert(monitor, "Generating code: " + getProject(), 8);
-		
-		
-		
+
 		// clean old code
 		GeneratorUtils.removeGeneratedCode(project, "src-gen/**");
 		subMon.worked(0);
-		
+
 		// create necessary folders
 		try {
 			createFoldersIfNecessary(project, subMon.split(1));
@@ -119,18 +141,22 @@ public class SimSGBuilder extends IncrementalProjectBuilder {
 		metaModelFolder = getProject().getFolder(DEFAULT_METAMODEL_LOCATION);
 //		modelFolder  = getProject().getFolder(DEFAULT_MODEL_LOCATION);
 		subMon.worked(1);
-		
+
 		// create models
 		runModelBuilders(project, monitor);
 		subMon.worked(2);
-		
+
 		// build metamodel code
-		for(IResource resource : metaModelFolder.members()) {
-			if(WorkspaceHelper.isFile(resource) && resource.getName().endsWith(".ecore")) {
-				Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().putIfAbsent("ecore", new EcoreResourceFactoryImpl());
+		for (IResource resource : metaModelFolder.members()) {
+			if (WorkspaceHelper.isFile(resource) && resource.getName().endsWith(".ecore")) {
+				
+				final String platformURI = "platform:/resource/" + project.getName() + "/" + resource.getProjectRelativePath();
+				
+				Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().putIfAbsent("ecore",
+						new EcoreResourceFactoryImpl());
 				ResourceSet rs = new ResourceSetImpl();
 				rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
-				Resource modelResource = rs.getResource(URI.createURI("platform:/resource/"+project.getName()+"/"+resource.getProjectRelativePath()), true);
+				Resource modelResource = rs.getResource(URI.createURI(platformURI), true);
 				EPackage metaModel = (EPackage) modelResource.getContents().get(0);
 				modelResource.unload();
 				try {
@@ -139,139 +165,146 @@ public class SimSGBuilder extends IncrementalProjectBuilder {
 					logger.error("Could not generate genmodel. Error: \n" + e.getMessage());
 					return;
 				}
-				
+
 			}
 		}
 		subMon.worked(3);
-		
+
 		boolean foundPatterns = false;
 		boolean foundRules = false;
-		
+
 		final Registry packageRegistry = new EPackageRegistryImpl();
 		// build eMoflon api code
-		for(IResource resource : metaModelFolder.members()) {
-			if(WorkspaceHelper.isFile(resource) && resource.getName().endsWith(".xmi")) {
+		for (IResource resource : metaModelFolder.members()) {
+			if (WorkspaceHelper.isFile(resource) && resource.getName().endsWith(".xmi")) {
 				GTRuleSet gtRules = null;
 				try {
 					Resource xmiResource = GeneratorUtils.loadXmi(resource);
 					Object content = xmiResource.getContents().get(0);
 					EcoreUtil.resolveAll(xmiResource);
-					if(content instanceof GTRuleSet) {
-						gtRules = (GTRuleSet)content;
-						resource.copy(project.getFile("src-gen/"+project.getName().replace(".", "/")+"/api/gt-rules.xmi").getFullPath(), false, monitor);
+					if (content instanceof GTRuleSet) {
+						gtRules = (GTRuleSet) content;
+						resource.copy(project.getFile(gtRulesLocation).getFullPath(), false,
+								monitor);
 						foundRules = true;
 					}
-					if(content instanceof IBeXPatternSet) {
-						resource.copy(project.getFile("src-gen/"+project.getName().replace(".", "/")+"/api/ibex-patterns.xmi").getFullPath(), false, monitor);
+					if (content instanceof IBeXPatternSet) {
+						resource.copy(project.getFile(ibexPatternsLocation).getFullPath(), false,
+								monitor);
 						foundPatterns = true;
 					}
 				} catch (IOException e) {
 					logger.error("Could not load resource. Error: \n" + e.getMessage());
 				}
-				
-				if(gtRules == null)
+
+				if (gtRules == null)
 					continue;
-				
-				IFolder apiPackage = project.getFolder("src-gen/"+project.getName().replace(".", "/")+"/api");
-				//final Registry packageRegistry = gtRules.eResource().getResourceSet().getPackageRegistry();
+
+				IFolder apiPackage = project.getFolder(apiResourcesLocation);
+				// final Registry packageRegistry =
+				// gtRules.eResource().getResourceSet().getPackageRegistry();
 				IBeXUtils.findAllEPackages(gtRules, packageRegistry);
-				
+
 				IBeXUtils.generateAPI(project, apiPackage, gtRules, packageRegistry);
 				updateManifest(project, this::processManifestForPackage);
 			}
 		}
 		subMon.worked(7);
-		
+
 		// build HiPE engine code
-		if(foundPatterns) {
-			IFolder packagePath = project.getFolder(project.getName().replace(".", "/"));
-			IBeXUtils.collectEngineBuilderExtensions().forEach(ext->ext.run(project, packagePath.getProjectRelativePath()));
+		if (foundPatterns) {
+			IFolder packagePath = project.getFolder(projectName.replace(".", "/"));
+			IBeXUtils.collectEngineBuilderExtensions()
+					.forEach(ext -> ext.run(project, packagePath.getProjectRelativePath()));
 		}
 		subMon.worked(8);
-		
+
 		// build SimSG API code
-		if(foundRules) {
-			IFolder apiPackage = project.getFolder("src-gen/"+project.getName().replace(".", "/")+"/api");
-			String apiPackageName = project.getName()+".api";
-			String classPrefix = URI.createFileURI(project.getName().replace(".", "/")).lastSegment();
+		if (foundRules) {
+			IFolder apiPackage = project.getFolder(apiResourcesLocation);
+			String apiPackageName = project.getName() + ".api";
+			String classPrefix = URI.createFileURI(projectName.replace(".", "/")).lastSegment();
 			classPrefix = Character.toUpperCase(classPrefix.charAt(0)) + classPrefix.substring(1);
 			EClassifiersManager ecManager = IBeXUtils.createEClassifierManager(packageRegistry);
-			SimSGAPIBuilder.buildAPI(apiPackage.getFile(classPrefix+"SimSGApi.java"), apiPackageName, classPrefix, ecManager);
+			SimSGAPIBuilder.buildAPI(apiPackage.getFile(classPrefix + "SimSGApi.java"), apiPackageName, classPrefix,
+					ecManager);
 		}
 		subMon.worked(9);
 	}
-	
-	protected void generateMetaModelCode(IProject project, IFile modelFile, EPackage metaModel) throws Exception {		
-		String pluginID = project.getName();
+
+	protected void generateMetaModelCode(IProject project, IFile modelFile, EPackage metaModel) throws Exception {
+		final String pluginID = project.getName();
+		final String metamodelName = metaModel.getName();
 
 		Monitor monitor = BasicMonitor.toMonitor(new NullProgressMonitor());
 		EcoreImporter importer = new EcoreImporter();
 		importer.setModelFile(modelFile);
 		importer.computeEPackages(monitor);
 		importer.adjustEPackages(monitor);
-		
-		for(EPackage ePackage : importer.getEPackages()) {
-			if(ePackage.getName().equals(metaModel.getName())) {
+
+		for (EPackage ePackage : importer.getEPackages()) {
+			if (ePackage.getName().equals(metamodelName)) {
 				importer.getEPackageConvertInfo(ePackage).setConvert(true);
-			}else {
+			} else {
 				importer.getEPackageConvertInfo(ePackage).setConvert(false);
 			}
-		}			
-		
-		importer.setGenModelContainerPath(new Path(pluginID).append("model"));
+		}
+
+		importer.setGenModelContainerPath(new Path(pluginID).append(DEFAULT_METAMODEL_LOCATION));
 		importer.setGenModelFileName(importer.computeDefaultGenModelFileName());
 		importer.prepareGenModelAndEPackages(monitor);
 
 		GenModel genModel = importer.getGenModel();
-		genModel.setModelDirectory(project.getName() + "/gen/");
-		
-	    Set<GenPackage> removals = genModel.getGenPackages().stream().filter(pkg -> !pkg.getEcorePackage().getName().equals(metaModel.getName())).collect(Collectors.toSet());
+		genModel.setModelDirectory(projectGenFolder);
+
+		Set<GenPackage> removals = genModel.getGenPackages().stream()
+				.filter(pkg -> !pkg.getEcorePackage().getName().equals(metamodelName)).collect(Collectors.toSet());
 		removals.forEach(pkg -> genModel.getGenPackages().remove(pkg));
 		genModel.getUsedGenPackages().addAll(removals);
-		
+
 		Map<String, URI> pack2genMapEnv = EcorePlugin.getEPackageNsURIToGenModelLocationMap(false);
 		Map<String, URI> pack2genMapTarget = EcorePlugin.getEPackageNsURIToGenModelLocationMap(true);
 
-		// create dummy genmodels or else the genpackages can not be found and thus persisted
-		for(GenPackage gp : removals) {
+		// create dummy genmodels or else the genpackages can not be found and thus
+		// persisted
+		for (GenPackage gp : removals) {
 			// search first in environment in case that the genmodel is exported as plugin
 			URI genURI = pack2genMapEnv.get(gp.getNSURI());
-			if(genURI == null)
+			if (genURI == null)
 				genURI = pack2genMapTarget.get(gp.getNSURI());
 			ResourceSet rs = new ResourceSetImpl();
 			Resource createResource = rs.createResource(genURI);
 			createResource.load(null);
-			if(createResource.isLoaded()) {
+			if (createResource.isLoaded()) {
 				GenModel newGen = (GenModel) createResource.getContents().get(0);
 				genModel.getUsedGenPackages().remove(gp);
 				genModel.getUsedGenPackages().add(newGen.getGenPackages().get(0));
-			}
-			else {
+			} else {
 				GenModel fakeGen = GenModelFactory.eINSTANCE.createGenModel();
 				fakeGen.setModelPluginID(gp.getEcorePackage().getNsPrefix());
 				fakeGen.getGenPackages().add(gp);
 				genModel.eResource().getContents().add(fakeGen);
 			}
 		}
-		
+
 		genModel.setGenerateSchema(true);
 		genModel.setCanGenerate(true);
-	    genModel.reconcile();
+		genModel.reconcile();
 
 		EcoreUtil.resolveAll(importer.getGenModelResourceSet());
-	    genModel.eResource().save(Collections.emptyMap());
-	    
+		genModel.eResource().save(Collections.emptyMap());
+
 		Generator generator = GenModelUtil.createGenerator(genModel);
-	    generator.setInput(genModel);
+		generator.setInput(genModel);
 		generator.generate(genModel, GenBaseGeneratorAdapter.MODEL_PROJECT_TYPE, monitor);
-		GeneratorUtils.removeGeneratedCode(project, "model/*.xsd");
+		GeneratorUtils.removeGeneratedCode(project, DEFAULT_METAMODEL_LOCATION + "/*.xsd");
 		importer.dispose();
 	}
-	
+
 	private void runModelBuilders(final IProject project, final IProgressMonitor monitor) {
-		IBeXUtils.collectModelBuilderExtensions().forEach(ext->ext.run(project, monitor));
+		IBeXUtils.collectModelBuilderExtensions().forEach(ext -> ext.run(project, monitor));
 	}
-	
+
 	private void updateManifest(final IProject project, final BiFunction<IProject, Manifest, Boolean> updateFunction) {
 		try {
 			new ManifestFileUpdater().processManifest(project, manifest -> updateFunction.apply(project, manifest));
@@ -279,10 +312,12 @@ public class SimSGBuilder extends IncrementalProjectBuilder {
 			logger.error("Failed to update MANIFEST.MF.");
 		}
 	}
-	
+
 	private boolean processManifestForPackage(IProject project, Manifest manifest) {
 		List<String> dependencies = new ArrayList<String>();
-		dependencies.addAll(Arrays.asList("org.emoflon.ibex.common", "org.emoflon.ibex.gt", "org.simsg.core", "SimulationDefinition"));
+
+		dependencies.addAll(
+				Arrays.asList(PACKAGE_IBEX_COMMON, PACKAGE_IBEX_GT, PACKAGE_SIMSG_CORE, PACKAGE_SIMULATION_DEFINITION));
 		IBeXUtils.collectEngineExtensions().forEach(engine -> dependencies.addAll(engine.getDependencies()));
 
 		boolean changedBasics = ManifestFileUpdater.setBasicProperties(manifest, project.getName());
@@ -301,21 +336,24 @@ public class SimSGBuilder extends IncrementalProjectBuilder {
 	private static void createFoldersIfNecessary(final IProject project, final IProgressMonitor monitor)
 			throws CoreException {
 		final SubMonitor subMon = SubMonitor.convert(monitor, "Creating folders within project " + project, 4);
-
+		
+		final String srcGenLocation = "src-gen";
+		final String apiResourceLocation = "src-gen/" + project.getName().replace(".", "/") + "/api";
+		final String apiMatchesLocation = apiResourceLocation + "/matches";
+		final String apiRulesLocation = apiResourceLocation + "/rules";
+		
 		WorkspaceHelper.createFolderIfNotExists(WorkspaceHelper.getSourceFolder(project), subMon.split(1));
 		WorkspaceHelper.createFolderIfNotExists(WorkspaceHelper.getBinFolder(project), subMon.split(1));
 		WorkspaceHelper.createFolderIfNotExists(WorkspaceHelper.getModelFolder(project), subMon.split(1));
 		WorkspaceHelper.createFolderIfNotExists(WorkspaceHelper.getGenFolder(project), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("src-gen"), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("src-gen/"+project.getName().replace(".", "/")+"/api"), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("src-gen/"+project.getName().replace(".", "/")+"/api/matches"), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("src-gen/"+project.getName().replace(".", "/")+"/api/rules"), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("instances"), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("instances/simulation_definitions"), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("instances/simulation_results"), subMon.split(1));
-		WorkspaceHelper.createFolderIfNotExists(project.getFolder("instances/simulation_instances"), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(srcGenLocation), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(apiResourceLocation), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(apiMatchesLocation), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(apiRulesLocation), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(DEFAULT_INSTANCES_LOCATION), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(DEFAULT_DEFINITION_LOCATION), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(DEFAULT_RESULTS_LOCATION), subMon.split(1));
+		WorkspaceHelper.createFolderIfNotExists(project.getFolder(DEFAULT_INSTANCES_LOCATION), subMon.split(1));
 	}
-
-	
 
 }
