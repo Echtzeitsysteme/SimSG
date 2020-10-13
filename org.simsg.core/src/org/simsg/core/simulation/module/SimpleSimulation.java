50375f6afc329e8785c2ddcdf0c5ce6c49f1fa72
SebastianE <sebastian.ehmes@gmail.com>
2020-10-13 14:24:27 +0200

    changed simple simulation to not depend on rule rate annotations

--- org.simsg.core/src/org/simsg/core/simulation/module/SimpleSimulation.java
+++ org.simsg.core/src/org/simsg/core/simulation/module/SimpleSimulation.java
@@ -1,5 +1,6 @@
 package org.simsg.core.simulation.module;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -20,11 +21,6 @@ import org.simsg.core.simulation.Simulation;
 
 public class SimpleSimulation extends Simulation {
 	
-	protected boolean randomRuleOrder = true;
-	protected boolean useRuleRates = true;
-	
-	private Map<String, Double> staticRuleRates = new HashMap<>();
-	
 	private Random random = new Random();
 	
 	public SimpleSimulation(String modelName, final BackendContainer backend) {
@@ -36,43 +32,21 @@ public class SimpleSimulation extends Simulation {
 	}
 	
 	public void randomizeRuleOrder(boolean activate) {
-		randomRuleOrder = activate;
+		
 	}
 
 	public void useRuleRates(boolean activate) {
-		useRuleRates = activate;
+		
 	}
 	
 	@Override
 	public void initialize() {
 		super.initialize();
-		for(IBeXRule rule : ibexModel.getRuleSet().getRules().stream()
-				.filter(rule ->rule.getProbability() != null)
-				.filter(rule -> rule.getProbability().getDistribution().getType() == IBeXDistributionType.STATIC)
-				.collect(Collectors.toList())) {
-			staticRuleRates.put(rule.getName(), state.getStaticProbability(rule.getName()).get());
-		}
 	}
 	
 	@Override
 	public void initializeClocked() {
 		super.initializeClocked();
-		for(IBeXRule rule : ibexModel.getRuleSet().getRules().stream()
-				.filter(rule ->rule.getProbability() != null)
-				.filter(rule -> rule.getProbability().getDistribution().getType() == IBeXDistributionType.STATIC)
-				.collect(Collectors.toList())) {
-			staticRuleRates.put(rule.getName(), state.getStaticProbability(rule.getName()).get());
-		}
-	}
-	
-	private ConcurrentLinkedQueue<String> generatePatternQueue() {
-		return new ConcurrentLinkedQueue<String>(staticRuleRates.keySet());
-	}
-	
-	private ConcurrentLinkedQueue<String> generateRndPatternQueue(){
-		List<String> rndPatternList = new LinkedList<String>(staticRuleRates.keySet());
-		Collections.shuffle(rndPatternList);
-		return new ConcurrentLinkedQueue<String>(rndPatternList);
 	}
 
 	@Override
@@ -85,30 +59,16 @@ public class SimpleSimulation extends Simulation {
 			state.clearEvents();
 		}
 		
-		ConcurrentLinkedQueue<String> patternQueue = null;
-		if(randomRuleOrder) {
-			patternQueue = generateRndPatternQueue();
-		}else {
-			patternQueue = generatePatternQueue();
-		}
-		String current = patternQueue.poll();
-		if(current == null) return;
+		List<String> candidates = new ArrayList<String>(ibexModel.getRuleSet().getRules().stream()
+				.map(rule -> rule.getName())
+				.filter(rule -> state.getMatchCount(rule)>0)
+				.collect(Collectors.toList()));
 		
-		if(useRuleRates) {
-			if(!staticRuleRates.containsKey(current)) {
-				return;
-			}
-			double reactionRate = staticRuleRates.get(current);
-			double pRule = 1.0 - Math.pow((1.0-reactionRate), state.getMatchCount(current));
-			double rnd = random.nextDouble();
-			if(rnd <= pRule) {
-				state.enqueueEvent(new Event(state.getTime()+1, current));
-			}
-		}else {
-			if(state.getMatchCount(current) != 0) {
-				state.enqueueEvent(new Event(state.getTime()+1, current));
-			}
+		if(candidates.size()<=0) {
+			state.setTime(state.getTime()+1);
+			return;
 		}
+		state.enqueueEvent(new Event(state.getTime()+1, candidates.get(random.nextInt(candidates.size()))));
 	}
 
 	@Override
